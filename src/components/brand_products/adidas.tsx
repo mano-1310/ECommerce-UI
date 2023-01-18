1da7072caf7bba69734e6457cae1b1e8b928a7b8
mano-1310 <s.mano1310@gmail.com>
2023-01-18 17:17:02 +0530

    products added

--- src/components/brand_products/adidas.tsx
+++ src/components/brand_products/adidas.tsx
@@ -1,21 +1,106 @@
 import React from 'react'
-
+const products = [
+  {
+    "id": 1,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": "450rs"
+  },
+  {
+    "id": 2,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": ""
+  },
+  {
+    "id": 3,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": ""
+  },
+  {
+    "id": 4,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": ""
+  },
+  {
+    "id": 5,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": ""
+  },
+  {
+    "id": 6,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": ""
+  },
+  {
+    "id": 7,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": "450rs"
+  },
+  {
+    "id": 8,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": ""
+  },
+  {
+    "id": 9,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": ""
+  },
+  {
+    "id": 10,
+    "product_name": "Black T-shirt",
+    "img_url": 'https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg',
+    "price": ""
+  },
+]
 const adidas = () => {
   return (
-    <div className="min-h-screen">
-      <div className="px-10 pb-6 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-6 gap-5">
+    <div className="min-h-screen flex">
+      <div className="px-10 pb-6 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-5">
         {
-          <div className="rounded bg-white overflow-hidden shadow-lg">
-            <img className="w-full h-[200px]" src="https://www.freeflys.com/wp-content/uploads/2022/02/adidas-product-testing.jpg" alt="Model1" />
-            <div className="px-6 py-4">
-              <div className="font-bold text-xl text-center mb-2">Adidas T-Shirt</div>
-              <p className="text-gray-700 text-base">
-              </p>
+          products.map((product, index) =>
+            <div className="rounded bg-white overflow-hidden shadow-lg" key={index}>
+              <img className="w-full h-[200px]" src={product.img_url} alt="Model1" />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl text-center mb-2">{product.product_name}</div>
+                <div>
+                  <p className="text-gray-700 text-base">
+                  </p>
+                  <label htmlFor="size" className="font-semibold py-1">Size</label>
+                  <select name="size" id="sze" className="w-full shadow-sm">
+                    <option>S</option>
+                    <option selected>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                    <option>XXl</option>
+                  </select>
+                </div>
+                <div className="pt-2">
+                  <label htmlFor="quantity" className="font-semibold">Quantity
+                    <input type="number" defaultValue={1} className="w-full shadow-sm h-[30px] px-2" />
+                  </label>
+                </div>
+                <div className="pt-2 text-xl font-bold">
+                  <h3>Price : {product.price} </h3>
+                </div>
+              </div>
+              <div className="justify-center flex gap-3 p-2">
+                <button className="py-1 w-full rounded text-white hover:bg-slate-700 bg-black" title='Add to Cart'>cart</button>
+                <button className="py-1 w-full rounded text-white hover:bg-slate-400 bg-black">Buy</button></div>
             </div>
-            <div className="justify-center flex p-2"><button className="py-2 w-full rounded text-white hover:bg-slate-900 bg-black">cart</button></div>
-          </div>
-
+          )
         }
+        <div >
+          <a className="text-xl" href="/adidas">See More....</a>
+        </div>
       </div>
     </div>
   )
--- src/components/brand_products/zara.tsx
+++ src/components/brand_products/zara.tsx
@@ -1,8 +1,108 @@
 import React from 'react'
-
+const products = [
+  {
+    "id": 1,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": "450rs"
+  },
+  {
+    "id": 2,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": ""
+  },
+  {
+    "id": 3,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": ""
+  },
+  {
+    "id": 4,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": ""
+  },
+  {
+    "id": 5,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": ""
+  },
+  {
+    "id": 6,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": ""
+  },
+  {
+    "id": 7,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": "450rs"
+  },
+  {
+    "id": 8,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": ""
+  },
+  {
+    "id": 9,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": ""
+  },
+  {
+    "id": 10,
+    "product_name": "Black T-shirt",
+    "img_url": '',
+    "price": ""
+  },
+]
 const zara = () => {
   return (
-    <div className="min-h-screen">zara</div>
+    <div className="min-h-screen flex">
+      <div className="px-10 pb-6 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-3 lg:grid-cols-5 xl:grid-cols-5 gap-5">
+        {
+          products.map((product, index) =>
+            <div className="rounded bg-white overflow-hidden shadow-lg" key={index}>
+              <img className="w-full h-[200px]" src={product.img_url} alt="zara" />
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl text-center mb-2">{product.product_name}</div>
+                <div>
+                  <p className="text-gray-700 text-base">
+                  </p>
+                  <label htmlFor="size" className="font-semibold py-1">Size</label>
+                  <select name="size" id="sze" className="w-full shadow-sm">
+                    <option>S</option>
+                    <option selected>M</option>
+                    <option>L</option>
+                    <option>XL</option>
+                    <option>XXl</option>
+                  </select>
+                </div>
+                <div className="pt-2">
+                  <label htmlFor="quantity" className="font-semibold">Quantity
+                    <input type="number" defaultValue={1} className="w-full shadow-sm h-[30px] px-2" />
+                  </label>
+                </div>
+                <div className="pt-2 text-xl font-bold">
+                  <h3>Price : {product.price} </h3>
+                </div>
+              </div>
+              <div className="justify-center flex gap-3 p-2">
+                <button className="py-1 w-full rounded text-white hover:bg-slate-700 bg-black" title='Add to Cart'>cart</button>
+                <button className="py-1 w-full rounded text-white hover:bg-slate-400 bg-black">Buy</button></div>
+            </div>
+          )
+        }
+        <div >
+          <a className="text-xl" href="/adidas">See More....</a>
+        </div>
+      </div>
+    </div>
   )
 }
 
